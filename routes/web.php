07b26ccb9514486c435d4f8eecae1f44260ce952
IqbalIslamay <iqbalislamay27@gmail.com>
2021-07-08 08:41:10 +0700

    3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +20,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class,'index']);
+Route::get('/home', [HomeController::class,'index']);
+
+Route::prefix('product')->group(function(){
+    Route::get('/list-product', [ProductController::class,'product']);
 });
+
+Route::get('news', function () {
+    return view('news',[NewsController::class, 'news' => 'news','topic' => 'educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19']);
+});
+
+Route::prefix('program')->group(function(){
+    Route::get('/daftar-program', [ProgramController::class,'program']);
+});
+
+Route::get('/about-us', [AboutController::class,'about']);
+
+
+Route::resource('contact', ContactController::class);
